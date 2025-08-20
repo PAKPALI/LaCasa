42f976c284fd9982fd7cbb4881057ae2b5088132
PAKPALI <77837289+PAKPALI@users.noreply.github.com>
2025-08-20 19:48:34 +0000

    push

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,48 +2,45 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>LaCasa &mdash; Bouger simplement</title>
-
-    <!-- Bootstrap CSS via CDN -->
+    <title>LaCasa-Bouger simplement</title>
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
-    <!-- Assets locaux -->
-    <link rel="stylesheet" href="{{ asset('LaCasa/fonts/icomoon/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('LaCasa/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('LaCasa/css/magnific-popup.css') }}">
-    <link rel="stylesheet" href="{{ asset('LaCasa/css/jquery-ui.css') }}">
-    <link rel="stylesheet" href="{{ asset('LaCasa/css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('LaCasa/css/owl.theme.default.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('LaCasa/css/bootstrap-datepicker.css') }}">
-    <link rel="stylesheet" href="{{ asset('LaCasa/css/mediaelementplayer.css') }}">
-    <link rel="stylesheet" href="{{ asset('LaCasa/css/animate.css') }}">
-    <link rel="stylesheet" href="{{ asset('LaCasa/fonts/flaticon/font/flaticon.css') }}">
-    <link rel="stylesheet" href="{{ asset('LaCasa/css/fl-bigmug-line.css') }}">
-    <link rel="stylesheet" href="{{ asset('LaCasa/css/aos.css') }}">
-    <link rel="stylesheet" href="{{ asset('LaCasa/css/style.css') }}">
-
-    <!-- Vite -->
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/fonts/icomoon/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/css/jquery-ui.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/css/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/css/mediaelementplayer.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/fonts/flaticon/font/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/css/fl-bigmug-line.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/css/aos.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('LaCasa/css/style.css') }}">
     @vite(['resources/css/app.css', 'resources/js/layouts/app.js'])
 </head>
+
 <body>
     <div id="app"></div>
 
-    <!-- Scripts locaux -->
-    <script src="{{ asset('LaCasa/js/jquery-3.3.1.min.js') }}"></script>
-    <script src="{{ asset('LaCasa/js/jquery-migrate-3.0.1.min.js') }}"></script>
-    <script src="{{ asset('LaCasa/js/jquery-ui.js') }}"></script>
-    <script src="{{ asset('LaCasa/js/popper.min.js') }}"></script>
-    <script src="{{ asset('LaCasa/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('LaCasa/js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('LaCasa/js/mediaelement-and-player.min.js') }}"></script>
-    <script src="{{ asset('LaCasa/js/jquery.stellar.min.js') }}"></script>
-    <script src="{{ asset('LaCasa/js/jquery.countdown.min.js') }}"></script>
-    <script src="{{ asset('LaCasa/js/jquery.magnific-popup.min.js') }}"></script>
-    <script src="{{ asset('LaCasa/js/bootstrap-datepicker.min.js') }}"></script>
-    <script src="{{ asset('LaCasa/js/aos.js') }}"></script>
-    <script src="{{ asset('LaCasa/js/main.js') }}"></script>
+    <!-- Scripts -->
+    <script src="{{ secure_asset('LaCasa/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ secure_asset('LaCasa/js/jquery-migrate-3.0.1.min.js') }}"></script>
+    <script src="{{ secure_asset('LaCasa/js/jquery-ui.js') }}"></script>
+    <script src="{{ secure_asset('LaCasa/js/popper.min.js') }}"></script>
+    <script src="{{ secure_asset('LaCasa/js/bootstrap.min.js') }}"></script>
+    <script src="{{ secure_asset('LaCasa/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ secure_asset('LaCasa/js/mediaelement-and-player.min.js') }}"></script>
+    <script src="{{ secure_asset('LaCasa/js/jquery.stellar.min.js') }}"></script>
+    <script src="{{ secure_asset('LaCasa/js/jquery.countdown.min.js') }}"></script>
+    <script src="{{ secure_asset('LaCasa/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ secure_asset('LaCasa/js/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ secure_asset('LaCasa/js/aos.js') }}"></script>
+    <script src="{{ secure_asset('LaCasa/js/main.js') }}"></script>
 </body>
 </html>
