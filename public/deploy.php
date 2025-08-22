fb3fab5ef3290fbb695c92b4b3242aecf3b2ee55
PAKPALI <77837289+PAKPALI@users.noreply.github.com>
2025-08-22 20:27:15 +0000

    first deploy and see log

---
+++ public/deploy.php
@@ -0,0 +1,11 @@
+<?php
+// Vérifier qu’on reçoit un POST
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(403);
+    exit('Forbidden');
+}
+
+// Exécuter le script de déploiement
+exec('/home/u151875458/domains/lacasaweb.com/public_html/test/deploy.sh > /dev/null 2>&1 &');
+
+echo "Déploiement lancé.";
