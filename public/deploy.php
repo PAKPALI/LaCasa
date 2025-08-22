3e6e6a6a36a21ab428bed8cf6a86683802f96ab0
PAKPALI <77837289+PAKPALI@users.noreply.github.com>
2025-08-22 20:38:42 +0000

    first deploy and see log

--- public/deploy.php
+++ public/deploy.php
@@ -9,3 +9,10 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 exec('/home/u151875458/domains/lacasaweb.com/public_html/test/deploy.sh > /dev/null 2>&1 &');
 
 echo "Déploiement lancé.";
+//v
+
+// deploy.php de test
+
+// file_put_contents(__DIR__.'/test.log', "Webhook reçu à ".date('Y-m-d H:i:s')."\n", FILE_APPEND);
+// echo "Webhook reçu.";
+
