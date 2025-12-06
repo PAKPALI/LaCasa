76b68c1ed5688bd96d2a0fd23bfb9fb7c9c60e76
PAKPALI <77837289+PAKPALI@users.noreply.github.com>
2025-12-06 21:16:40 +0000

    work on seo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,7 +39,7 @@
 
     <!-- Assets locaux -->
     <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
+    <link rel="icon" href="{{ config('app.url') }}/logo/logo.jpeg" type="image/png">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
